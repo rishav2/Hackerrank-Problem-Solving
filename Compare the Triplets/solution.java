eb2860ef7cafd7f35c8ff482e6a9eb989c91ba96
rishav2 <41112605+rishav2@users.noreply.github.com>
2019-01-20 18:35:14 +0530

    .java

---
+++ Compare the Triplets/solution.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+       ArrayList<Integer> integers = new ArrayList<>();
+       int alice =0;
+       int bob=0;
+        for(int i=0;i<a.size();i++){
+            for(int j=i;j<=i;j++)
+            {
+                if(a.get(i)>b.get(j))
+                {
+                    alice+=1;
+                }
+                else if(a.get(i)<b.get(j)){
+            
+                    bob+=1;
+                }
+                else{
+                    
+                   }
+
+
+            }
+            
+        }
+        integers.add(alice);
+        integers.add(bob);
+        return integers;
+
+    } 
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
