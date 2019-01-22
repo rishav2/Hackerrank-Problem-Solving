11f5ec842ed0b2bb45a5cf25def222b77fad66f4
rishav2 <41112605+rishav2@users.noreply.github.com>
2019-01-22 14:31:48 +0530

    .java

--- Time Conversion/solution.java
+++ Time Conversion/solution.java
@@ -70,18 +70,4 @@ return null;
     }
 }
 
-    private static final Scanner scan = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String s = scan.nextLine();
-
-        String result = timeConversion(s);
-
-        bw.write(result);
-        bw.newLine();
-
-        bw.close();
-    }
-}
+ 
